0c02f57c70481357eb541df765011e5abf529e06
Joren Van Hocht <vanhochtjoren@gmail.com>
2016-05-28 13:49:51 +0200

    Bug fix creating new model and apply PSR

--- database/Seeds/UsersTableSeeder.php
+++ database/Seeds/UsersTableSeeder.php
@@ -1,38 +1,50 @@
-<?php namespace jorenvanhocht\Blogify\database\Seeds;
+<?php
+
+namespace jorenvanhocht\Blogify\database\Seeds;
 
 use Carbon\Carbon;
 use Illuminate\Database\Seeder;
 use jorenvanhocht\Blogify\Models\Role;
-use App\User;
 use \Illuminate\Support\Facades\Hash;
 
-class UsersTableSeeder extends Seeder {
+class UsersTableSeeder extends Seeder
+{
 
+    /**
+     * The id of the admin role
+     *
+     * @var
+     */
     private $admin_role;
+
+    /**
+     * Admin user information
+     *
+     * @var mixed
+     */
     private $admin;
 
     public function __construct()
     {
         $this->admin = config('blogify.blogify.admin_user');
 
-        // Get the id of the admin role
         $role = Role::where('name', '=', 'Admin')->first();
         $this->admin_role = $role->id;
     }
 
     public function run()
     {
-        $user = app()->make(config(('auth.model')));
+        $user = app()->make(config(('blogify.auth_model')));
         $user->create([
-            'hash'          => blogify()->makeHash('users', 'hash', true),
-            'lastname'      => $this->admin['name'],
-            'firstname'     => $this->admin['firstname'],
-            'username'      => $this->admin['username'],
-            'email'         => $this->admin['email'],
-            'password'      => Hash::make($this->admin['password']),
-            'role_id'       => $this->admin_role,
-            'created_at'    => Carbon::now(),
-            'updated_at'    => Carbon::now()
+            'hash' => blogify()->makeHash('users', 'hash', true),
+            'lastname' => $this->admin['name'],
+            'firstname' => $this->admin['firstname'],
+            'username' => $this->admin['username'],
+            'email' => $this->admin['email'],
+            'password' => Hash::make($this->admin['password']),
+            'role_id' => $this->admin_role,
+            'created_at' => Carbon::now(),
+            'updated_at' => Carbon::now()
         ]);
     }
 
